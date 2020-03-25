7d08255c360d61b68cacb1d158ef13e013cc87a3
@itelofilho <itelofilho@gmail.com>
2020-03-25 13:01:34 -0300

    Updates

---
+++ precache-manifest.35bca6356e61dadbb71a2504fc0616e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1690d7636fd3a3d3403ff7fce454d30b",
+    "url": "/upnid-for-speed/index.html"
+  },
+  {
+    "revision": "6b69d0982f1eef5a741d",
+    "url": "/upnid-for-speed/static/css/main.68a95506.chunk.css"
+  },
+  {
+    "revision": "7459cbc8da3f6321feb5",
+    "url": "/upnid-for-speed/static/js/2.e1dfaeb9.chunk.js"
+  },
+  {
+    "revision": "ccfa566c1f8347f163d87e9920122859",
+    "url": "/upnid-for-speed/static/js/2.e1dfaeb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b69d0982f1eef5a741d",
+    "url": "/upnid-for-speed/static/js/main.1e557648.chunk.js"
+  },
+  {
+    "revision": "c277dac6737f58a99bff",
+    "url": "/upnid-for-speed/static/js/runtime-main.ab41cc80.js"
+  },
+  {
+    "revision": "56be0a4e7be4b661072436aa22f66f64",
+    "url": "/upnid-for-speed/static/media/car.56be0a4e.png"
+  },
+  {
+    "revision": "d0d2cd1752d215c4df68e76e976ec629",
+    "url": "/upnid-for-speed/static/media/road.d0d2cd17.gif"
+  }
+]);
